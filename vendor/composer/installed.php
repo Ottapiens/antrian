6bdff58d7f0201ae973161bb4977596bbe00da53
ottapien <ottapien@kali.otta>
2025-11-15 08:14:33 -0600

    Add Snyk, Dependabot, and CI/CD setup

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ottapiens/antrian',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '722cea95bb4f914bbfbb86d646d9d0755605fa17',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ottapiens/antrian' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '722cea95bb4f914bbfbb86d646d9d0755605fa17',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
